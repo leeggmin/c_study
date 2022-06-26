d9d74cf7727cb742f62c132deeca35b786c8c954
이지민 <wlals060330@gmail.com>
2022-06-26 23:24:11 +0900

    codingtest

---
+++ CodingTest/도토리키제기(구조체).c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+struct student {
+	char name[20];
+	double hei;
+};
+
+int main() {
+
+	int n;
+	double heiavg = 0; //평균 키
+	struct student member[10] = { '\0',0 };
+	scanf("%d", &n); //학생 수 입력받기
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s %lf", member[i].name, &member[i].hei);
+		heiavg += member[i].hei;
+	}
+	heiavg /= (double)n;
+
+	for (int i = 0; i < n; i++) {
+		printf("%s ", member[i].name);
+		if (member[i].hei > heiavg)
+			printf("UP\n");
+		else if (member[i].hei == heiavg)
+			printf("SAME\n");
+		else
+			printf("DOWN\n");
+	}
+
+	return 0;
+}
